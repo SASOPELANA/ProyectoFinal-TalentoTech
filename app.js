884362f5572a5aae06053b6335d4090cdf32b3a7
Sergio Alejandro <sopekof@gmail.com>
2024-12-21 13:34:07 -0300

    Ultimos cambios

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-// variable global
+
+// variable global para almacenar los productos seleccionados
 let carrito = [];
 
-const agregarAlCarrito = (nombre, precio) => {
-  //agregar el producto como un objecto al carrito
-  carrito.push({ nombre, precio });
+const agregarAlcarrito = (nombre,precio) =>{
+    //agregar el producto como un objeto al carrito
+    carrito.push({nombre,precio})
 
-  // Actualizar contador del carrito de compras
-  actualizarContador();
+    // actualizar el contador visual del carrito
+    actualizarContador()
+    // muestra un alerta de confirmacion
+    alert(`Agregaste : ${nombre} al carrito`)
+}
 
-  // muestra un alerta de confirmacion
-  alert(`Agregaste:  ${nombre} al carrito. `);
-};
 
-// funcion para el contdor del carrito
-const actualizarContador = () => {
-  // Actualizar el contenido html del id contador-carrito
-  document.getElementById("contador-carrito").textContent = carrito.length;
-};
+// funcion para actualizar el contador del carrito
+const actualizarContador = ()=>{
+    //cambiamos el contenido del HTML con el ID contador-carrito
+    document.getElementById("contador-carrito").textContent = carrito.length
 
-// Guardar el contenido del carrito en el almacenamiento local antes de cerrar la pagina
-window.addEventListener("beforeunload", () => {
-  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+// Guarda el contenido del carrito en el almacenamiento local antes de cerrar la pagina
+
+window.addEventListener("beforeunload",()=>{
+localStorage.setItem("carrito",JSON.stringify(carrito))
 });
